98c3bd9e8603123185674b7449836283d084c385
gus <53922692+guswithahoodie@users.noreply.github.com>
2025-11-06 10:57:51 -0600

    Update

--- demo_project/infra/ecs-deploy/ecr.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_ecr_repository" "app" {
-  name                 = "${var.project}-${var.env}"
-  image_tag_mutability = "MUTABLE"
-  tags                 = { Name = "${var.project}-${var.env}-ecr" }
-}
