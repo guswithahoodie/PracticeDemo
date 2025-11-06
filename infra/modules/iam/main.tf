25a59c775690bceb12357a250557ec6df6b472c5
gus <53922692+guswithahoodie@users.noreply.github.com>
2025-11-06 12:50:05 -0600

    Update fomatting

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -4,8 +4,13 @@ resource "aws_iam_role" "ec2_role" {
 }
 
 data "aws_iam_policy_document" "ec2_assume_role" {
-  statement { actions = ["sts:AssumeRole"]
-    principals { type = "Service" identifiers = ["ec2.amazonaws.com"] }
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
   }
 }
 
