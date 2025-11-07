7e8b0c1a3cc3fc150909049867716eaebe302c39
gus <53922692+guswithahoodie@users.noreply.github.com>
2025-11-07 17:02:33 -0600

    Update connectivity issue

--- infra/modules/ec2/outputs.tf
+++ infra/modules/ec2/outputs.tf
@@ -10,3 +10,7 @@ output "app_url" {
   description = "Public URL for the deployed Django app"
   value       = "http://${aws_instance.app.public_ip}:8000"
 }
+
+output "security_group_id" {
+  value = aws_security_group.app_sg.id
+}
