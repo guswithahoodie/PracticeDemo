24122eab8df1beb169dd14c1d81544e1be911d02
gus <53922692+guswithahoodie@users.noreply.github.com>
2025-11-08 09:30:47 -0600

    Update public ip variables

--- infra/modules/ec2/outputs.tf
+++ infra/modules/ec2/outputs.tf
@@ -1,5 +1,5 @@
 output "instance_public_ip" {
-  value = aws_instance.app.public_ip
+  value = aws_eip.ec2_eip.public_ip
 }
 
 output "instance_id" {
@@ -8,7 +8,7 @@ output "instance_id" {
 
 output "app_url" {
   description = "Public URL for the deployed Django app"
-  value       = "http://${aws_instance.app.public_ip}:8000"
+  value       = "http://${aws_eip.ec2_eip.public_ip}:8000"
 }
 
 output "security_group_id" {
